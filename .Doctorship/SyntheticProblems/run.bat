62ed15882471ece9b41f93972fef5ba29ec38b51
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-08-06 11:31:00 +0200

    bat runner

---
+++ .Doctorship/SyntheticProblems/run.bat
@@ -0,0 +1,8 @@
+@echo off
+set experiment=%1
+set configuration=%2
+
+php generate-queue.php -e %experiment% -c %configuration%
+php execute.php -c %configuration%
+php generate-standings.php -e %experiment% -c %configuration%
+php generate-plots.php -e %experiment% -c %configuration%
